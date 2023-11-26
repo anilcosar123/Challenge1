cfe73ea49dabbd9a588c60a19560396b7bae9835
Anil <cosaranil@hotmail.com>
2023-11-26 01:30:08 +0100

    merged codes

--- final-code.py
+++ final-code.py
@@ -3,40 +3,33 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
-#import plotly.figure_factory as ff
-#import plotly.graph_objs as go
 
-
-# Global variable to track the current tab
-current_tab = 'Homepage'
-
+# To show overall data
 def plot_interactive_bar_chart_EDA():
     
     df_eda =pd.read_csv(r"C:\Users\anilp\Project_Files\23-11-2023 11h59m03s\df_all_eda.csv", header=0)
     
-    # Define category labels
+    # Define category thresholds to calculate overall data
     category_labels = ['Low', 'Mid', 'High']
-    # Define the category thresholds
     num_categories = 3
     
-    #Define the min and max of HR
+    # Find the min and max of EDA
     eda_min = df_eda['EDA'].min()
     eda_max = df_eda['EDA'].max()
     
+    # Setting thresholds according to max - min divided by number of categories
     thresholds = [eda_min]
     thresholds.extend([eda_min + (i + 1) * ((eda_max - eda_min) / num_categories) for i in range(num_categories - 1)])
     thresholds.append(eda_max)
 
-    # Categorize values
+    # Adding threshold labels to values in dataframe
     categories = pd.cut(df_eda['EDA'], bins=thresholds, labels=category_labels)
 
-    # Group by category and count occurrences
+    # Sorting the values respective to their categories into a new dataframe
     category_counts = categories.value_counts().reindex(category_labels, fill_value=0)
-
-    # Create a DataFrame with category labels and counts
     df_counts = pd.DataFrame({'Categories': category_counts.index, 'Counts': category_counts.values})
 
-    # Create a bar chart using Plotly Express
+    # Plotting a barchart to show overall data
     fig_eda = px.bar(df_counts, x='Categories', y=[thresholds[i + 1] - thresholds[i] for i in range(num_categories)], base=thresholds[:-1], title="Overall EDA Data")
     
     fig_eda.update_traces(
@@ -51,34 +44,33 @@ def plot_interactive_bar_chart_EDA():
     )
 
     st.plotly_chart(fig_eda)
-    
+
+# To show overall data
 def plot_interactive_bar_chart_HR():
     
     df_hr =pd.read_csv(r"C:\Users\anilp\Project_Files\23-11-2023 11h57m59s\df_all_hr.csv", header=0)
     
-    # Define category labels
+    # Define category thresholds to calculate overall data
     category_labels = ['Low', 'Mid', 'High']
-    # Define the category thresholds
     num_categories = 3
     
-    #Define the min and max of HR
+    # Find the min and max of HR
     hr_min = df_hr['HR'].min()
     hr_max = df_hr['HR'].max()
     
+    # Setting thresholds according to max - min divided by number of categories
     thresholds = [hr_min]
     thresholds.extend([hr_min + (i + 1) * ((hr_max - hr_min) / num_categories) for i in range(num_categories - 1)])
     thresholds.append(hr_max)
 
-    # Categorize values
+    # Adding threshold labels to values in dataframe
     categories = pd.cut(df_hr['HR'], bins=thresholds, labels=category_labels)
 
-    # Group by category and count occurrences
+    # Sorting the values respective to their categories into a new dataframe
     category_counts = categories.value_counts().reindex(category_labels, fill_value=0)
-
-    # Create a DataFrame with category labels and counts
     df_counts = pd.DataFrame({'Categories': category_counts.index, 'Counts': category_counts.values})
 
-    # Create a bar chart using Plotly Express
+    # Plotting a barchart to show overall data
     fig_hr = px.bar(df_counts, x='Categories', y=[thresholds[i + 1] - thresholds[i] for i in range(num_categories)], base=thresholds[:-1], title="Overall HR Data")
     
     fig_hr.update_traces(
@@ -94,176 +86,119 @@ def plot_interactive_bar_chart_HR():
 
     st.plotly_chart(fig_hr)
 
-# Function to plot interactive line charts for EDA and BVP
-def plot_interactive_line_charts(data, start_time_eda, end_time_eda, start_time_bvp, end_time_bvp):
+# To show daily data
+def plot_interactive_line_charts(data, start_time_eda, end_time_eda, start_time_hr, end_time_hr):
     
     st.write('Here you will see two graphs regarding your EDA. The first graph will show your EDA metrics before and after your gym session. The second graph will show your overall EDA levels. You can see three columns (low/medium/high) this will show how intense your workout was based on your EDA levels. So you can use this graph to interpret if you had a low, medium or high intensity gym session.')
     
-    # Filter data for EDA based on time range
+    # Takes Time column from dataframe
     filtered_data_eda = data[(data['Time'] >= start_time_eda) & (data['Time'] <= end_time_eda)]
     
     # Plot line chart for EDA
-    fig_eda = px.line(filtered_data_eda, x='Time', y='EDA', title="Line Chart for EDA")
+    fig_eda = px.line(filtered_data_eda, x='Time', y='EDA', title="Daily EDA Data")
     fig_eda.update_layout(
         xaxis_title='Time',
         yaxis_title='EDA',
         showlegend=True
     )
     
+    # Runs the daily plot first then overall plot
     st.plotly_chart(fig_eda)
     plot_interactive_bar_chart_EDA()
     
     st.write('Here you will see two graphs regarding your HR. The first graph will show your HR metrics before and after your gym session. The second graph will show your overall HR levels. You can see three columns (low/medium/high) this will show how intense your workout was based on your HR levels. So you can use this graph to interpret if you had a low, medium or high intensity gym session.')
-
-    # Filter data for BVP based on time range
-    filtered_data_bvp = data[(data['Time'] >= start_time_bvp) & (data['Time'] <= end_time_bvp)]
     
-    # Plot line chart for BVP
-    fig_hr = px.line(filtered_data_bvp, x='Time', y='HR', title="Line Chart for HR")
+    # Takes Time column from dataframe
+    filtered_data_hr = data[(data['Time'] >= start_time_hr) & (data['Time'] <= end_time_hr)]
+    
+    # Plot line chart for HR
+    fig_hr = px.line(filtered_data_hr, x='Time', y='HR', title="Daily HR Data")
     fig_hr.update_layout(
         xaxis_title='Time',
         yaxis_title='HR',
         showlegend=True
     )
     
+    # Runs the daily plot first then overall plot
     st.plotly_chart(fig_hr)
     plot_interactive_bar_chart_HR()
 
-# Function to manage food consumption history in Tab 2
-def manage_food_history():
-    st.subheader('Food Consumption Tracker')
+
+def homepage(mean_calorie, mean_weight, health_data=None):
+    st.subheader("Welcome to the Homepage!")
+    st.write(f"Average Calorie Intake: {mean_calorie} calories")
+    st.write(f"Average Weight: {mean_weight} kg")
+
+    if health_data:
+        st.subheader("Health Information")
+        
+        df_health_data = pd.DataFrame(health_data).T.reset_index()
+        df_health_data.columns = ['Date', 'Calories', 'Weight']
+
+        df_health_data['Date'] = pd.to_datetime(df_health_data['Date']).dt.date
+        st.dataframe(df_health_data)
+
+def popup():
+    st.subheader("Health Information")
     
-    # Create or load the food history data
-    if 'food_history' not in st.session_state:
-        st.session_state.food_history = pd.DataFrame(columns=['Datetime', 'Food', 'Calories'])
-    
-    # Input for the user to enter consumed food and calories
-    food_consumed = st.text_input('Enter the Consumed Food:')
-    calories_consumed = st.number_input('Enter the Calories Consumed:', min_value=0)
-    
-    # Button to add the input to the history
-    if st.button('Add to History'):
-        current_datetime = pd.Timestamp.now()
-        st.session_state.food_history = st.session_state.food_history.append({'Datetime': current_datetime, 'Food': food_consumed, 'Calories': calories_consumed}, ignore_index=True)
-        st.success(f"Added: {food_consumed} with {calories_consumed} calories at {current_datetime} to the history.")
+    selected_dates = st.multiselect("Select dates:", pd.date_range(start='2023-11-25', end='2023-12-31'))
 
-    # Expander to show and hide the history
-    with st.expander("Food Consumption History"):
-        st.write(st.session_state.food_history)
+    for date in selected_dates:
+        st.write(f"Date: {date}")
+        calorie_input = st.number_input("Enter your daily calorie intake:", min_value=0)
+        weight_input = st.number_input("Enter your weight (in kg):", min_value=0)
 
-    # Calculate and display average weekly calorie consumption
-    st.subheader('Average Weekly Calorie Consumption')
-    st.session_state.food_history['Datetime'] = pd.to_datetime(st.session_state.food_history['Datetime'])
-    weekly_calories_user = st.session_state.food_history.set_index('Datetime').resample('W-Mon')['Calories'].sum()
-    average_weekly_calories_user = weekly_calories_user.mean()
-    st.write(f'The average weekly calorie consumption based on user inputs is: {average_weekly_calories_user:.2f} calories.')
+        if st.button("Saved"):
+           
+            if 'health_data' not in st.session_state:
+                st.session_state.health_data = {}
 
-    # Calculate and display average weekly calorie consumption for the entire history
-    weekly_calories_total = st.session_state.food_history.resample('W-Mon', on='Datetime')['Calories'].sum()
-    average_weekly_calories_total = weekly_calories_total.mean()
-    st.write(f'The average weekly calorie consumption for the entire history is: {average_weekly_calories_total:.2f} calories.')
+            st.session_state.health_data[date] = {'calories': calorie_input, 'weight': weight_input}
+
+            st.success("Health information saved!")
+            
+def render_homepage():
+    mean_calorie = mean_weight = 0
+    health_data = getattr(st.session_state, 'health_data', {})
+
+    if health_data:
+        all_calories = [data['calories'] for data in health_data.values()]
+        all_weights = [data['weight'] for data in health_data.values()]
+        mean_calorie = sum(all_calories) / len(all_calories)
+        mean_weight = sum(all_weights) / len(all_weights)
+
+    homepage(mean_calorie, mean_weight, health_data)
+
+def render_health():
+    popup()
 
 
-
-    # Add content for Tab 3 as needed
-
-# Main Streamlit app
 def main():
-    global current_tab
-
+    
     # Read data from CSV file
     csv_file_path = r"C:\Users\anilp\Project_Files\testing\df_result.csv"
     data = pd.read_csv(csv_file_path)
 
-    # Convert 'Time' column to datetime format
+   # Convert 'Time' column to datetime format
     data['Time'] = pd.to_datetime(data['Time'])
-
-    # Extract the tab name from the URL
-    tab = st.experimental_get_query_params().get("tab", ["Homepage"])[0]
-
-    current_tab = tab  # Update the current tab
-
-    if st.button("Homepage"):
-         current_tab = 'Homepage'
-         st.experimental_set_query_params(tab=current_tab)
-         homepage_content()
-    
-    elif st.button("Gym"):
-         current_tab = 'Gym'
-         st.experimental_set_query_params(tab=current_tab)
-         gym_content(data)
-    
-    elif st.button("Health"):
-         current_tab = 'Health'
-         st.experimental_set_query_params(tab=current_tab)
-         health_content()
-    
-    elif st.button("Pop-up"):
-         current_tab = 'Pop-up'
-         st.experimental_set_query_params(tab=current_tab)
-         popup_content()
-
-
-
-    # Function for Homepage content
-@st.cache_resource(experimental_allow_widgets=True)
-def homepage_content():
-    
-    st.write('With our app you can see the data related to EDA and BVP while keeping a track of your calories and weight!')
-    st.divider()
-    st.write('The EDA stands for Electrodermal activity. EDA is used to measure the conductance of the skin, also known as sweat.')
-    st.write('HR stands for Heartrate, also known as the number of times your heart beats per minute. the HR is calculated by collecting the BVP signal (Blood volume pulse).BVP is measeaured by measuring the volume of blood that flows through the arteries where the sensor is placed, the E4 empathica wristband.')
-    
-    st.divider()
-    st.write('Average weekly calorie consumption(measured by last 7 inputs in dataframe): 123')
-    st.write('Average weekly weight(measured by last 7 inputs in dataframe): 123')
-    st.divider()
-
-    #st.write('Pop-up Button')
-    calendar = pd.DataFrame(index=['Week 1','Week 2','Week 3', 'Week 4'], columns=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
-    st.write(calendar)
-    
-    #st.markdown('<style>' + open('icons.css').read() + '</style>', unsafe_allow_html=True)
-    
-    # Add any additional content you want for the homepage
-    #st.write('Homepage Button')
-    
    
+    st.set_page_config(page_title="Health App", layout="wide")
 
+    tabs = ["Homepage", "Health", "Gym"]
+    tab_labels = ["Home", "Health", "Gym"]
 
-# Function for Gym content
-@st.cache_resource(experimental_allow_widgets=True)
-def gym_content(data):
-    
-    st.write('Welcome to your gym page! Here you will see your health metrics regarding your gym sessions.')
+    current_tab_index = st.sidebar.selectbox("Navigation", range(len(tabs)), format_func=lambda i: tab_labels[i])
 
-    # Call function for interactive line charts
-    plot_interactive_line_charts(data, data['Time'].min(), data['Time'].max(), data['Time'].min(), data['Time'].max())
+    current_tab = tabs[current_tab_index]
 
-    
-
-# Function for Health content
-@st.cache_resource(experimental_allow_widgets=True)
-def health_content():
-    st.write('This is the health content.')
-    #manage_food_history()
-    
-    calorie = pd.DataFrame(index=['Input values:'], columns=['Your calorie intake on Monday was : 1', 'Your calorie intake on Tuesday was : 2', 'Your calorie intake on Wednesday was : 3',
-    'Your calorie intake on Thursday was : 4', 'Your calorie intake on Friday was : 5', 'Your calorie intake on Saturday was : 6', 'Your calorie intake on Sunday was : 7'])
-
-
-    weight = pd.DataFrame(index=['Input values'], columns=['Your weight on Monday was : ', 'Your weight on Tuesday was : ', 'Your weight on Wednesday was : ',
-    'Your weight on Thursday was : ', 'Your weight on Friday was : ', 'Your weight on Saturday was : ', 'Your weight on Sunday was : '])
-    
-    st.write(calorie, 'Average calorie consumption for your week was : ')
-    
-    st.write(weight, 'Avegare weight for your week was : ')
-
-# Function for Pop-up content
-@st.cache_resource(experimental_allow_widgets=True)
-def popup_content():
-    st.write('This is the pop-up content.')
+    if current_tab == "Homepage":
+        render_homepage()
         
+    elif current_tab == "Health":
+        render_health()
+        
+    elif current_tab == "Gym":
+        plot_interactive_line_charts(data, data['Time'].min(), data['Time'].max(), data['Time'].min(), data['Time'].max())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
